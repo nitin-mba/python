b208a20709986833953c2d7bc6a0abaa22fae639
NITIN RAJ <113101797+nitin-mba@users.noreply.github.com>
2022-12-07 15:55:49 +0530

    Add files via upload

---
+++ Factorial_via_iteration.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  4 13:19:36 2022
+
+@author: Deepak
+"""
+
+def factI(n):
+    result = 1
+    while n > 1:
+        result = result * n
+        n -= 1
+    return result
+
+factI(4)
